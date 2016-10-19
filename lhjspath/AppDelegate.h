700d20f38eb0fb010f76bb5619db2dfc3bd66c62
lleihai123 <lh@lhdeiMac.local>
2016-10-19 15:27:14 +0800

    Initial Commit

---
+++ lhjspath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lhjspath
+//
+//  Created by lh on 2016/10/19.
+//  Copyright © 2016年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lhjspath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lhjspath
+//
+//  Created by lh on 2016/10/19.
+//  Copyright © 2016年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
